a9b705d695f364d1feaf43cf2604aaf7c428ea5e
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-08-05 14:35:24 -0400

    feat/se crearon tablas: user, imageprofile, post, postimage, likes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,32 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+//Este archivo contiene configuraciones específicas para el entorno de producción.
+//Incluye configuraciones para optimización de archivos, minificación y otras mejoras de rendimiento.
+
+// Importa la función 'merge' del módulo 'webpack-merge' que se usa para combinar configuraciones de webpack.
+const { merge } = require("webpack-merge");
+
+// Importa la configuración común desde 'webpack.common.js'.
+const common = require("./webpack.common.js");
+
+// Importa el plugin 'dotenv-webpack' que carga variables de entorno desde un archivo .env.
+const Dotenv = require("dotenv-webpack");
+
+// Exporta la configuración final de webpack para el entorno de producción, combinada con la configuración común.
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: '/'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  // Establece el modo de webpack en 'production' para optimizaciones adecuadas para producción.
+  mode: "production",
+
+  // Configura la salida de los archivos empaquetados.
+  output: {
+    // Define la ruta pública para referenciar los archivos en el servidor.
+    publicPath: "/",
+  },
+
+  // Lista de plugins que se usarán en el entorno de producción.
+  plugins: [
+    // Plugin para cargar variables de entorno desde un archivo .env.
+    new Dotenv({
+      safe: true, // Verifica que todas las variables necesarias están definidas en .env.
+      systemvars: true, // Permite usar variables del sistema además de las definidas en el archivo .env.
+    }),
+  ],
 });
