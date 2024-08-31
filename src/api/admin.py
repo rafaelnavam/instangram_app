3059f728536e50e52c46f7eea59a01a96ab56df3
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-08-31 14:20:48 -0400

    commet/se restrigio la vista admin backend

--- src/api/admin.py
+++ src/api/admin.py
@@ -5,17 +5,29 @@ from .models import db, User, ProfileImage, Post, PostImage, Likes
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
-    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='Admin Management', template_mode='bootstrap3')
+    return """<!DOCTYPE html>
+                <html lang="es">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>403 - Acceso Denegado</title>
+                </head>
+                <body>
+                    <h1>403 - Acceso Denegado</h1>
+                </body>
+                </html>"""
+
+    # app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
+    # app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    # admin = Admin(app, name='Admin Management', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(ProfileImage, db.session))
-    admin.add_view(ModelView(Post, db.session))
-    admin.add_view(ModelView(PostImage, db.session))
-    admin.add_view(ModelView(Likes, db.session))
+    # # Add your models here, for example this is how we add a the User model to the admin
+    # admin.add_view(ModelView(User, db.session))
+    # admin.add_view(ModelView(ProfileImage, db.session))
+    # admin.add_view(ModelView(Post, db.session))
+    # admin.add_view(ModelView(PostImage, db.session))
+    # admin.add_view(ModelView(Likes, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/utils.py
+++ src/api/utils.py
@@ -31,10 +31,21 @@ def generate_sitemap(app):
                 links.append(url)
 
     links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
-    return """
-        <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
-        <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
-        <p>Remember to specify a real endpoint path like: </p>
-        <ul style="text-align: left;">"""+links_html+"</ul></div>"
+    # return """
+    #     <div style="text-align: center;">
+    #     <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
+    #     <h1>Rigo welcomes you to your API!!</h1>
+    #     <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
+    #     <p>Remember to specify a real endpoint path like: </p>
+    #     <ul style="text-align: left;">"""+links_html+"</ul></div>"
+    return """<!DOCTYPE html>
+                <html lang="es">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>403 - Acceso Denegado</title>
+                </head>
+                <body>
+                    <h1>403 - Acceso Denegado</h1>
+                </body>
+                </html>"""
